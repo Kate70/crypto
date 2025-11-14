2965f8aa8afd7c401aef1aad72f7e049dae164b6
Kateryna Rudenko <rydakatrinr@gmail.com>
2025-11-14 17:55:41 +0200

    Initial commit: додала адаптивний header та мобільне меню

--- js/index.js
+++ js/index.js
@@ -2,11 +2,45 @@ const swiper = new Swiper('.agreements__swiper', {
   
   loop: true,
 
-    slidesPerView: 4,
+    
   spaceBetween: 20,
   
-  
+   breakpoints: {
+    380: {
+      slidesPerView: 1, 
+    },
+    480: {
+      slidesPerView: 2, 
+    },
+    768: {
+      slidesPerView: 3, 
+    },
+     1100: {
+      slidesPerView: 4, 
+    },
+  },
   scrollbar: {
     el: '.swiper-scrollbar',
   },
 });
+
+const items = document.querySelectorAll('.block__item');
+
+items.forEach(item => {
+    item.addEventListener('click', () => {       
+        
+        items.forEach(i => {
+            if (i !== item) i.classList.remove('active__item');
+        });       
+        item.classList.toggle('active__item');
+    });
+});
+
+
+const burger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+burger.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+});
+
