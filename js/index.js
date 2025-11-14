e19c126166f744d4da261e0537915432402536d5
Kate70 <rydakatrinr@gmail.com>
2025-11-14 07:56:10 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.agreements__swiper', { 
+  
+  loop: true,
+
+    slidesPerView: 4,
+  spaceBetween: 20,
+  
+  
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
